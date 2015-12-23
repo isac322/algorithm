d01a26312705b061f7c134056677fcc1573dbee4
유병훈 <isac322@naver.com>
2015-12-23 21:58:05 +0900

    boyer moore complete

--- algorithm/boyer-moore.h
+++ algorithm/boyer-moore.h
@@ -1,50 +1,81 @@
 #pragma once
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 namespace BoyerMoore {
-	std::unordered_map<char, int> map;
+	std::unordered_map<char, size_t> map;
+	std::vector<size_t> mached;
 
-	void initMap(const std::string &pattern) {
+	void init_map(const std::string &pattern) {
 		map.clear();
 
 		for (size_t i = 0; i < pattern.size(); i++) {
 			map[pattern[i]] = i;
 		}
 	}
-
-	// TODO
+	
 	int boyer_moore(const std::string &target, const std::string &pattern) {
-		initMap(pattern);
+		init_map(pattern);
 
 		const size_t &t_len = target.size(), &p_len = pattern.size();
-		size_t start = 0, t_index, p_index;
+		size_t index = p_len;
 
-		t_index = p_index = p_len - 1;
+		bool check;
 
-		while (t_index < t_len) {
-			if (p_index == -1) return start;
+		while (true) {
+			check = true;
 
-			if (target[t_index] != pattern[p_index]) {
-				t_index = start + p_len;
-				start++;
-
-				char next = target[t_index];
-				if (map.find(next) != map.end()) {
-					p_index = map[next];
+			for (size_t i = 1; i <= p_len; i++) {
+				if (target[index - i] != pattern[p_len - i]) {
+					check = false;
+					break;
 				}
-				else {
-					p_index = p_len - 1;
-				}
+			}
+
+			if (check) {
+				return index - p_len;
+			}
+			else if (index >= t_len) {
+				return -1;
 			}
 			else {
-				p_index--;
-				t_index--;
+				index += p_len - map[target[index]];
 			}
 		}
+	}
 
-		if (p_index == -1) return start;
-		else return -1;
+	std::vector<size_t>& boyer_moore_all(const std::string &target, const std::string &pattern) {
+		init_map(pattern);
+		mached.clear();
+
+		const size_t &t_len = target.size(), &p_len = pattern.size();
+		size_t index = p_len;
+
+		bool check;
+
+		while (true) {
+			check = true;
+
+			for (size_t i = 1; i <= p_len; i++) {
+				if (target[index - i] != pattern[p_len - i]) {
+					check = false;
+					break;
+				}
+			}
+
+			if (check) {
+				mached.push_back(index - p_len);
+
+				index += p_len - map[target[index]];
+			}
+			else if (index >= t_len) {
+				return mached;
+			}
+			else {
+				index += p_len - map[target[index]];
+			}
+		}
 	}
 }
 
