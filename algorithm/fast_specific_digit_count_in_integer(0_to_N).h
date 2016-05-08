84fdb5c2685902bb986e87c9a7bdd06f628cf53c
유병훈 <isac322@naver.com>
2016-05-08 13:29:01 +0900

    fix typo

--- algorithm/fast_specific_digit_count_in_integer(0_to_N).h
+++ algorithm/fast_specific_digit_count_in_integer(0_to_N).h
@@ -6,7 +6,7 @@ https://www.acmicpc.net/problem/11170
 */
 int solve(int n, int t) {
 	if (n < 0) return 0;
-	int cnt = 1, prev = 1;
+	int cnt = t == 0 ? 1 : 0, prev = 1;
 	for (int i = 10; i <= n; i *= 10) {
 		if (n % i / prev == t) cnt += ((n - i) / i) * prev + 1 + n % prev;
 		else cnt += (n / i)*prev;
