336b210dce33b4919efb3ac8fd1bee8fa8e0c6e9
isac322 <isac322@naver.com>
2016-08-16 23:04:37 +0900

    add counting size function on Union-Find structure

--- algorithm/union-find.h
+++ algorithm/union-find.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <cstddef>
 #include <vector>
-#include <algorithm>
 
 struct UnionFind {
-	std::vector<std::size_t> parent, depth;
+	std::vector<std::size_t> parent, depth, size;
 
-	UnionFind(std::size_t len) :parent(len), depth(len, 1) {
+	UnionFind(std::size_t len) :parent(len), depth(len, 1), size(len, 1) {
 		for (std::size_t i = 0; i < len; i++) parent[i] = i;
 	}
 
@@ -16,12 +15,21 @@ struct UnionFind {
 		else return p = find(p);
 	}
 
-	void uni(std::size_t p, std::size_t q) {
+	std::size_t uni(std::size_t p, std::size_t q) {
 		p = find(p);
 		q = find(q);
-		if (p == q) return;
-		else if (depth[p] > depth[q]) parent[q] = p;
-		else if (depth[p] != depth[q]) parent[p] = q;
-		else parent[q] = p, depth[p]++;
+		if (p == q) return size[p];
+		else if (depth[p] > depth[q]) {
+			parent[q] = p;
+			return size[p] += size[q];
+		}
+		else if (depth[p] != depth[q]) {
+			parent[p] = q;
+			return size[q] += size[p];
+		}
+		else {
+			parent[q] = p, depth[p]++;
+			return size[p] += size[q];
+		}
 	}
 };
